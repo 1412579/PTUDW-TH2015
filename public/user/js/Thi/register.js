c1a1436accec15239709b14a7007d7c13f1065f9
Nguyễn Hoàng Thi <1412520@student.hcmus.edu.vn>
2018-04-28 03:19:48 +0700

    toggle appropriate tabs on register modals

--- public/user/js/Thi/register.js
+++ public/user/js/Thi/register.js
@@ -2,4 +2,12 @@ $('document').ready(function() {
 	$('.datepicker').datepicker({
 	    format: 'mm/dd/yyyy'
 	});
+
+	$('#registerBtn').on('click', function() {
+		$('.nav-tabs a[href="#register"]').tab('show')
+	});
+
+		$('#loginBtn').on('click', function() {
+		$('.nav-tabs a[href="#log-in"]').tab('show')
+	});
 })
