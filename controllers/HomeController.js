c2aae8e121696dfece737bec10264c87be6483da
Nguyễn Hoàng Thi <1412520@student.hcmus.edu.vn>
2018-06-09 22:32:03 +0700

    add product model with methods to get newest, best selling, most wiewed, get by brand, get by product type

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,18 +1,40 @@
-var category = require('../model/category.js')
+var category = require('../model/category.js');
+var product = require('../model/product.js')
 
 var homeController =  
 {
 	index: function(req, res)
 	{
 		let categories = [];
-		category.getDetailedCategory()
-			.then((result) => {
+		let promises = [];
+		promises.push(category.getDetailedCategory());
+		promises.push(product.getNewestProducts(10)); 
+		promises.push(product.getBestSellerProducts(10));
+		promises.push(product.getMostViewdProducts(10));
+		Promise.all(promises)
+			.then(function(result) {
+				console.log(result);
 				var model = {
 						title: 'Trang chủ',
-						categories: result
+						categories: result[0],
+						newestProducts: result[1],
+						bestSellerProducts: result[2],
+						mostViewedProducts: result[3]
 					};
 					res.render('home', model);
+			})
+			.catch(function(error) {
+				console.log(error);
+				res.end();
 			});
+		// category.getDetailedCategory()
+		// 	.then((result) => {
+		// 		var model = {
+		// 				title: 'Trang chủ',
+		// 				categories: result
+		// 			};
+		// 			res.render('home', model);
+		// 	});
 	}
 }
 
