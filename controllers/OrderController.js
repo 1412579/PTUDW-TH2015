d871b160bbb38da03638a6f2d4ab1a025d805ba1
Vũ Minh Trí <1412579@student.hcmus.edu.vn>
2018-06-23 15:57:28 +0700

    commit edit trang thai don hang order

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -14,4 +14,35 @@ router.get('/list', (req, res) =>{
     .catch(err => console.log(err));
 });
 
+router.get('/edit/:id', (req, res) => {
+    let orderdetail;
+    Order.getById(req.params.id)
+    .then(result => {
+        orderdetail = result;
+        return Order.getProducts(req.params.id);
+    })
+    .then(result => {
+        res.render('admin/edit-order',{
+            layout: 'main-admin',
+            orderdetail: orderdetail,
+            products: result,
+            message: req.flash('messageCate')[0]
+        }); 
+    })
+    .catch(err=>console.log(err));
+});
+
+router.post('/edit/:id', (req, res) => {
+    let data = {
+        id: req.params.id,
+        status: req.body.status
+    }
+    Order.updateStatusById(data)
+    .then(() => {
+        req.flash('messageCate', 'Đã cập nhật đơn hàng thành công!');
+        res.redirect(req.get('referer'));
+    })
+    .catch(err=>console.log(err));
+});
+
 module.exports = router;
--- model/order.js
+++ model/order.js
@@ -15,7 +15,7 @@ var Category = {
     },
 	getById: (id)=>{
         return new Promise((resolve,reject)=>{
-            pool.query(`select * from category where id=${id}`, function(err, result){
+            pool.query(`select orders.created_at as timeorder, orders.id as orderid, users.address as useraddress, users.fullname as username, users.phone as userphone,order_status.name as status,order_status.id as statusid, orders.total as total from orders,users,order_status where orders.order_status_id = order_status.id and orders.user_id = users.id and orders.id=${id}`, function(err, result){
                 if (err){
                     reject(err);
                 }
@@ -23,10 +23,21 @@ var Category = {
                     resolve(result.rows[0]);
             });
         });
-	},
-	updateById: function(cateInfo){
+    },
+    getProducts: (id)=>{
         return new Promise((resolve,reject)=>{
-            var query = `update category set parent_id = ${cateInfo.parent_id},name = '${ cateInfo.name }', alias = '${ cateInfo.alias }', updated_at = '${ cateInfo.updated_at }', hide = ${ cateInfo.ishide } where id = ${ cateInfo.id}`;
+            pool.query(`select * from order_details where order_id=${id}`, function(err, result){
+                if (err){
+                    reject(err);
+                }
+                else
+                    resolve(result.rows);
+            });
+        });
+	},
+	updateStatusById: function(cateInfo){
+        return new Promise((resolve,reject)=>{
+            var query = `update orders set order_status_id = ${cateInfo.status} where id = ${ cateInfo.id}`;
             console.log(query);
             pool.query(query, function(err, result){
                 if (err){
